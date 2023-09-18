b4ec4b770aa327b5c45ba9b32d579a242489ee41
Nate Martin <thenatemartin@gmail.com>
2023-09-18 15:56:18 -0400

    Added default board layout

--- pyGammon.py
+++ pyGammon.py
@@ -1,9 +1,21 @@
 import numpy as np
 
+
 class pyGammon:
 
     def __init__(self):
-        self.board = np.zeros(24, dtype="int8")
+        self.board = np.zeros(24, dtype="i1")
+        self.jail = np.zeros(2, dtype="i1")
+        self.rolled_off = np.zeros(2, dtype="i1")
+
+        self.board[0] = -2
+        self.board[5] = 5
+        self.board[7] = 3
+        self.board[11] = -5
+        self.board[12] = 5
+        self.board[16] = -3
+        self.board[18] = -5
+        self.board[23] = 2
 
     def __str__(self):
         out = ""
